55d21db9ea3ac79ed47467724645f9360f4044ce
ScherbakovDV <shcherbakov_dmitry@bk.ru>
2018-11-25 16:55:22 +0300

    Третье задание почти готово, 2 номер сложный

--- Task 3/src/source.kt
+++ Task 3/src/source.kt
@@ -1,41 +1,105 @@
 package Tasks
 
+import java.util.TreeSet
 
-fun generatePermutations(arr: Array<Int>): ArrayList<Array<Int>> {
+
+fun generatePermutations() {
+    val num = readLine()?.toInt() ?: return
     var result: ArrayList<Array<Int>> = ArrayList()
-    var input = arr
+    var input = Array(num, { 0 })
+    val s = readLine()?.split(" ") ?: return
+    for (i in 0 until num) {
+        input[i] = s[i].toInt()
+    }
     input.sort()
-    input.reverse()
-    var sequences = Array(arr.size, { 0 })
-    var directions = Array(arr.size, { 1 })
-    var flag = true
-    while (flag) {
-        result.add(input)
-        var i = input.size - 1
-        while ( (i > 0) && ( ( (directions[i] == 1) && (sequences[i] == i))
-                        || ( (directions[i] == -1) && (sequences[i] == 0) ) ) ) {
+    result.add(input.clone())
+    while (true) {
+        var i = input.size - 2
+        while (i != -1 && (input[i] >= input[i + 1])) {
             i--
         }
-        flag = i > 0
-        if (flag) {
-            sequences[i] += directions[i]
-            for (j in (i + 1) until input.size) {
-                directions[j] = -directions[j]
-            }
-            var j = input.size - 1
-            while ((j > 0) && (input[j] != i + 1)) {
-                j--
-            }
-            val buf = input[j]
-            input[j] = input[j+directions[i]].also{input[j+directions[i]] = buf}
+        if (i == -1) {
+            break
         }
+        var j = input.size - 1
+        while (input[i] >= input[j]) {
+            j--
+        }
+        input[i] = input[j].also { input[j] = input[i] }
+        var l = i + 1
+        var r = input.size - 1
+        while (l < r) {
+            input[l] = input[r].also { input[r] = input[l] }
+            l++
+            r--
+        }
+        result.add(input.clone())
     }
-    return result
+    for (arr in result) {
+        println(arr.joinToString(" "))
+    }
 }
 
+
+fun findRange() {
+
+}
+
+
+fun maxRemoveableVertexes() {
+    var num = readLine()?.toInt() ?: return
+    //Основные массивы
+    var tree: Array<ArrayList<Int>> = Array(num, { ArrayList<Int>() })
+    var visited = Array(num, { false })
+    var subtreesSizes = Array(num, { 0 })
+
+    //Поиск в глубину c подсчётом размеров поддеревьев для каждой вершины
+    fun countSizes(vertex: Int): Int {
+        visited[vertex] = true
+        var subsizesNumber = 0
+        for (i in tree[vertex]) {
+            if (!visited[i]) {
+                subsizesNumber += countSizes(i)
+            }
+        }
+        subtreesSizes[vertex] = subsizesNumber + 1
+        return subsizesNumber + 1
+    }
+
+    //Основная часть
+    for (i in 0 until (num - 1)) {
+        val input = (readLine() ?: return).split(" ")
+        val a = input[0].toInt() - 1
+        val b = input[1].toInt() - 1
+        tree[a].add(b)
+        tree[b].add(a)
+    }
+    var answer = 0
+    if (num % 2 == 1) {
+        answer = -1
+    } else {
+        countSizes(0)
+        for (i in 0 until num) {
+            if (subtreesSizes[i] % 2 == 0) {
+                answer++
+            }
+        }
+        answer--
+    }
+    println(answer)
+}
 
 fun main(argc: Array<String>) {
-    for (permutation in generatePermutations(arrayOf(1, 2, 3))) {
-        println(permutation.joinToString(" "))
-    }
+//    generatePermutations()
+/*
+3
+4 10 15 24 26
+0 9 12 20
+5 18 22 30
+*/
+
+    findRange()
+
+//    maxRemoveableVertexes()
 }
+
