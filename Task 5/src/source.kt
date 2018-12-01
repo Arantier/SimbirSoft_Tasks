ae62e609b62e97428de29342238b6a490b5f3fef
ScherbakovDV <shcherbakov_dmitry@bk.ru>
2018-12-01 23:16:13 +0300

    7.2 сделан. И чё я над ним так долго думал...

--- Task 5/src/source.kt
+++ Task 5/src/source.kt
@@ -1,37 +1,81 @@
+import kotlin.math.pow
 import kotlin.math.sqrt
+import kotlin.math.truncate
 
 fun checkForCross(rect1: Array<Int>, rect2: Array<Int>): Boolean {
-    return (((rect1[0] in (rect2[0]+1) until rect2[2]) || (rect1[2] in (rect2[0] +1) until rect2[2]))
-            && ((rect1[1] in (rect2[1]+1) until rect2[3]) || (rect1[3] in (rect2[1]+1) until rect2[3])))
+    return (((rect1[0] in (rect2[0] + 1) until rect2[2]) || (rect1[2] in (rect2[0] + 1) until rect2[2]))
+            && ((rect1[1] in (rect2[1] + 1) until rect2[3]) || (rect1[3] in (rect2[1] + 1) until rect2[3])))
 }
 
 
-fun findSquares(num:Int):Int{
+fun findSumOfSquares(num: Int): Int {
+    fun Int.checkForSquare(): Boolean {
+        return truncate(sqrt(this.toDouble())).pow(2).toInt() == this
+    }
 
-    fun findDividers(num:Int):Pair<ArrayList<Int>,ArrayList<Int>>{
-        var simpleDividers = Pair(ArrayList<Int>(),ArrayList<Int>())
-        for (i in 2..sqrt(num.toDouble()).toInt()){
-            if (num%i==0){
-                simpleDividers.first.add(i)
-                simpleDividers.second.add(num/i)
+
+    fun Int.splitOnAddents(num: Int): Array<Array<Int>> {
+        lateinit var addents: Array<Int>
+        val superAddents = ArrayList<Array<Int>>()
+        if (num < 0 || num > this) {
+            return superAddents.toTypedArray()
+        } else if (num == 1) {
+            addents = arrayOf(this)
+            superAddents.add(addents)
+            return superAddents.toTypedArray()
+        } else if (num == this) {
+            addents = Array(this, { 1 })
+            superAddents.add(addents)
+            return superAddents.toTypedArray()
+        }
+        //TODO:Всё равно неправильно
+        addents = Array(num + 1, { 1 })
+        addents[0] = this
+        superAddents.add(addents.sliceArray(0 until num))
+        while (addents.last() != 1) {
+            var ind = 0
+            for (i in this - 1 downTo 1)
+                if (addents[i] > 1) {
+                    ind = i
+                    break
+                }
+            addents[ind]--
+            addents[ind + 1]++
+            var sum = 0
+            for (i in ind + 1 until this) {
+                sum += addents[i]
+                addents[i] = 0
+            }
+            for (i in ind + 1 until this) {
+                if (addents[i - 1] < sum) {
+                    addents[i] = addents[i - 1]
+                    sum -= addents[i - 1]
+                } else {
+                    addents[i] = sum
+                    break
+                }
+            }
+            if (addents[num - 1] != 0 && addents[num] == 0) {
+                val arr = addents.sliceArray(0 until num)
+                superAddents.add(arr)
             }
         }
-        return simpleDividers
+        return superAddents.toTypedArray()
     }
 
-    if (sqrt(num.toDouble()).toInt()*sqrt(num.toDouble()).toInt() == num){
-        return 1
+
+    for (i in 1 until num) {
+        val addents = num.splitOnAddents(i)
+        if (addents.any { it.all { it.checkForSquare() } }) {
+            return i
+        }
     }
-
-    val dividers = findDividers(num)
-
-
-    return 0
+    return num
 }
 
 
 fun main(argv: Array<String>) {
     println(if (checkForCross(arrayOf(0, 0, 2, 2), arrayOf(1, 1, 3, 3))) "true" else "false")
     println(if (checkForCross(arrayOf(0, 0, 1, 1), arrayOf(1, 0, 2, 1))) "true" else "false")
-    findSquares(12)
+    println(findSumOfSquares(12))
 }
--- Task 7/src/source.kt
+++ Task 7/src/source.kt
@@ -15,109 +15,68 @@ fun isPalindrome(str: String): Boolean {
     }
 }
 
-fun Int.splitOnAddents(num: Int): Array<Array<Int>> {
-    var addents = ArrayList<Int>()
-    var superAddents = ArrayList<Array<Int>>()
-    for (i in 1..this) {
-        addents.add(0)
-    }
-    if (num < 0 || num > this) {
-        return superAddents.toTypedArray()
-    } else if (num == 1) {
-        addents[0] = this
-        addents.removeIf { it == 0 }
-        superAddents.add(addents.toTypedArray())
-        return superAddents.toTypedArray()
-    } else if (num == this) {
-        addents.addAll(Array(num, { 1 }))
-        superAddents.add(addents.toTypedArray())
-        return superAddents.toTypedArray()
-    }
-    addents[0] = this
-    while (addents[0] != 1) {
-        var ind = 0
-        for (i in this - 1 downTo 1)
-            if (addents[i] > 1) {
-                ind = i
-                break
-            }
-        addents[ind]--
-        addents[ind + 1]++
-        var sum = 0
-        for (i in ind + 1 until this) {
-            sum += addents[i]
-            addents[i] = 0
-        }
-        for (i in ind + 1 until this) {
-            if (addents[i - 1] < sum) {
-                addents[i] = addents[i - 1]
-                sum -= addents[i - 1]
-            } else {
-                addents[i] = sum
-                break
-            }
-        }
-        if (addents[num - 1] != 0 && addents[num] == 0) {
-            var arr = addents.clone() as ArrayList<Int>
-            arr.removeIf { it == 0 }
-            superAddents.add(arr.toTypedArray())
-        }
-    }
-    return superAddents.toTypedArray()
-}
 
-
-fun findSumOfSquares(num: Int): Int {
-    for (i in 1..num){
-        var addents = num.splitOnAddents(i)
-        for (v in addents){
-/*            if (v.all { sqrt(it.toDouble()).pow(2) == it.toDouble() }){
-                return v.size
-            }*/
-            if (v.all{it ->
-                        val a = sqrt(it.toDouble()).toInt().toDouble()
-                        val b = a.pow(2).toInt()
-                        val c = it
-                        b == c
-                    }) {
-                return v.size
-            }
-        }
-    }
-    return -1
-}
-
-
-fun allenGoneMad(){
-    fun recursiveDropPairs(arr:ArrayList<Int>):Int{
-        if (arr.isEmpty()){
-            return 0
-        } else if (arr[0]==arr[1]){
-            return recursiveDropPairs(arr.subList(2,arr.size) as ArrayList<Int>)
-        } else if (arr[arr.size-2] == arr.last()){
-            return recursiveDropPairs(arr.subList(0,arr.size-2) as ArrayList<Int>)
+fun checkForSplit(arr: ArrayList<Int>,
+                  leftSum: Int = 0,
+                  rightSum: Int = 0): Boolean {
+    if (arr.isEmpty()) {
+        return leftSum == rightSum
+    } else {
+        val new = arr.max()!!
+        arr.remove(new)
+        //Поскольку arr,max==null <=> arr.size==0, то можно рискнуть
+        return if (leftSum > rightSum) {
+            checkForSplit(arr, leftSum, rightSum + new)
         } else {
-            for (i in 1 until arr.size-2){
-                if (arr[i]==arr[i+1]){
+            checkForSplit(arr, leftSum + new, rightSum)
+        }
+    }
+}
+
+
+fun allenGoneMad() {
+    fun recursiveDropPairs(arr: ArrayList<Int>): Int {
+        if (arr.isEmpty()) {
+            return 0
+        } else {
+            if (arr[0] == arr[1]) {
+                return recursiveDropPairs(arr.subList(2, arr.size) as ArrayList<Int>)
+            } else if (arr[arr.size - 2] == arr.last()) {
+                return recursiveDropPairs(arr.subList(0, arr.size - 2) as ArrayList<Int>)
+            }
+            for (i in 1 until arr.size - 2) {
+                if (arr[i] == arr[i + 1]) {
                     arr.remove(arr[i])
-                    return min(i, arr.size-i+1) + recursiveDropPairs(arr)
+                    return min(i, arr.size - i + 1) + recursiveDropPairs(arr)
                 }
             }
-            //Дальше ищешь пересечения разделённых пар с минимальными расстояниями
-            //, удаляешь эти пары и добавляешь к итогу сумму расстояний - 1
-            //TODO:Есть проблема с выводом
+            var distance = arr.size
+            for (i in 1 until arr.size) {
+                val e1 = arr.indexOfLast { it == arr[i] }
+                val s2 = arr.indexOfFirst { it == arr[i - 1] }
+                if (i == e1 && (i - 1) == s2) {
+                    val s1 = arr.indexOfFirst { it == arr[i] }
+                    val e2 = arr.indexOfLast { it == arr[i - 1] }
+                    val newDist = e1 - s1 + e2 - s2
+                    if (newDist < distance) {
+                        distance = newDist
+                    }
+                }
+            }
+
         }
+        return -1
     }
+
     val num = readLine()?.toInt() ?: return
     val rawPairs = readLine()?.split(" ") ?: return
-    val pairs = Array(num*2,{it -> rawPairs[it].toInt()})
+    val pairs = Array(num * 2, { it -> rawPairs[it].toInt() })
 
 
 }
 
-//TODO: Серьёзно оптимизируй код, это просто
+
 fun main(argv: Array<String>) {
     println(isPalindrome(123321))
-
-    println(findSumOfSquares(121312))
+    println(checkForSplit(arrayListOf(2, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,10)))
 }
